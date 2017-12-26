fcbf8ebd7f6eb864a83626666feaf1f015fca22c
svava <svava@svava-PC>
2017-12-27 01:03:39 +0900

    lsmisced

--- OpenNetworkFolder/main.cpp
+++ OpenNetworkFolder/main.cpp
@@ -1,112 +1,17 @@
 
 #include <Windows.h>
 
-#include <Shlobj.h>
-#include <Shobjidl.h>
-#include <comdef.h>
-#include <comdefsp.h>
+
 
 #include <string>
 
 #include "../../TimedMessageBox/TimedMessageBox/TimedMessageBox.h"
+#include "../../lsMisc/RevealFolder.h"
+
 
 using namespace std;
+using namespace Ambiesoft;
 
-// http://www.kab-studio.biz/Programing/Codian/ShellExtension/05.html
-BOOL MakeList(IShellFolderPtr pSF, LPITEMIDLIST p_pFolderIDList)
-{
-	HRESULT		hRes;
-	ULONG		ulRetNo;
-	STRRET		stFileName;
-	LPITEMIDLIST	pFileIDList;
-	IShellFolderPtr	pCurFolder;
-	IEnumIDListPtr	pEnumIDList;
-	wstring		cPrintStr;
-
-	if (p_pFolderIDList != NULL)
-	{
-		//　IShellFolderにバインドします。
-		hRes = pSF->BindToObject(p_pFolderIDList, NULL, IID_IShellFolder, (LPVOID *)&pCurFolder);
-		if (hRes != NOERROR)
-			return TRUE;
-	}
-	else
-	{
-		//　デスクトップフォルダを指定します。
-		hRes = ::SHGetDesktopFolder(&pCurFolder);
-		if (hRes != NOERROR)
-			return TRUE;
-	}
-
-	//　IEnumIDListを取得します。
-	hRes = pCurFolder->EnumObjects(NULL //GetSafeHwnd()
-		, SHCONTF_NONFOLDERS | SHCONTF_INCLUDEHIDDEN | SHCONTF_FOLDERS, &pEnumIDList);
-	if (hRes != NOERROR)
-		return FALSE;
-
-	//　IEnumIDListからアイテムＩＤを取得していきます。
-	while (pEnumIDList->Next(1, &pFileIDList, &ulRetNo) == NOERROR)
-	{
-		//　ファイルパスの取得。
-		hRes = pCurFolder->GetDisplayNameOf(pFileIDList
-			, SHGDN_FORPARSING, &stFileName);
-		if (hRes != NOERROR)
-			break;
-
-		//　文字列の変換。
-		// cPrintStr = TFileName(pFileIDList, &stFileName);
-
-		// TRACE("%s\n", (LPCTSTR)cPrintStr);
-
-		CoTaskMemFree(pFileIDList);
-	}
-
-	//pCurFolder->Release();
-
-	return TRUE;
-}
-
-
-LPITEMIDLIST GetItemIDList(IShellFolderPtr pSF, wstring p_cFileStr)
-{
-	if (p_cFileStr.empty())
-		return NULL;
-
-	HRESULT		hRes;
-	ULONG		chEaten;	//文字列のサイズを受け取ります。
-	ULONG		dwAttributes;	//属性を受け取ります。
-	//OLECHAR		ochPath[MAX_PATH];	//ワイドバイト文字列です。
-	LPITEMIDLIST	pIDL;	//フォルダを示すアイテムＩＤです。
-
-	//　これをしないとインターフェイスはダメなのです。
-	// ::MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, p_cFileStr, -1, ochPath, MAX_PATH);
-
-	//　実際にITEMIDLISTを取得します。
-
-	hRes = pSF->ParseDisplayName(NULL, NULL, (LPWSTR)p_cFileStr.c_str(), &chEaten, &pIDL, &dwAttributes);
-
-	if (hRes != NOERROR)
-		pIDL = NULL;
-
-	return pIDL;	//取得したアイテムＩＤを返します。
-}
-
-BOOL DoReveal(LPCTSTR pFolder)
-{
-	IShellFolderPtr pSF;
-	HRESULT hRes = ::SHGetDesktopFolder(&pSF);
-	if (!pSF)
-		return FALSE;
-
-	LPITEMIDLIST pIIL = GetItemIDList(pSF, pFolder);
-	if (!pIIL)
-		return FALSE;
-
-	if (!MakeList(pSF, pIIL))
-		return FALSE;
-
-	return TRUE;
-}
 
 
 #define APPNAME L"OpenNetworkFolder"
@@ -162,7 +67,7 @@ int WINAPI wWinMain(
 			continue;
 		}
 
-		if (DoReveal(buff))
+		if (RevealFolder(buff))
 		{
 			message += L"OK";
 		}
